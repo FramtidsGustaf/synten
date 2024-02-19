a33facb3ac7cc3e407b466c6dee4f08bd7e48a95
framtidsgustaf <gustaf89@hotmail.se>
2024-02-19 21:11:59 +0100

    another fix in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/synth",
+	base: "/synten",
 	plugins: [
 		react({ babel: { plugins: [["module:@preact/signals-react-transform"]] } }),
 	],
