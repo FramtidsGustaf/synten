1cb9a0bc3f9e441a471deadf3c2ba66e0b239776
framtidsgustaf <gustaf89@hotmail.se>
2024-02-20 09:44:15 +0100

    new day new tries

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	console.log(mode);
 	return {
 		base: mode === "development" ? "" : "/synten",
 		plugins: [
