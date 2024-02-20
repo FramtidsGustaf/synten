43b0922169fb59c769a1f9126d1206099981201c
framtidsgustaf <gustaf89@hotmail.se>
2024-02-20 16:44:18 +0100

    åosdfk

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
 	return {
-		base: mode === "development" ? "" : "/synten",
+		base: "/synten/",
 		plugins: [
 			react({
 				babel: { plugins: [["module:@preact/signals-react-transform"]] },
 			}),
 		],
+		preview: {
+			port: 3000,
+			strictPort: true,
+		},
+		server: {
+			port: 3000,
+			strictPort: true,
+			host: true,
+			origin: "http://0.0.0.0:3000",
+		},
 	};
 });
