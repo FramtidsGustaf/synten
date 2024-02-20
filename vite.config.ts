25860f2c1f1d3c7cca9b07c1146102e6393a370e
framtidsgustaf <gustaf89@hotmail.se>
2024-02-20 16:49:48 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	return {
-		base: "/synten/",
+		base: "/synten",
 		plugins: [
 			react({
 				babel: { plugins: [["module:@preact/signals-react-transform"]] },
